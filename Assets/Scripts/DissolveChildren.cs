60c635eceb074a8a70ec3f995d4af0ef113322c8
Jarvis Lawson <123907462+PlatGX@users.noreply.github.com>
2024-03-06 21:54:14 -0600

    Created colored buttons that coordinate colored effects

--- Assets/Scripts/DissolveChildren.cs
+++ Assets/Scripts/DissolveChildren.cs
@@ -4,34 +4,77 @@ using UnityEngine;
 
 public class DissolveChildren : MonoBehaviour
 {
-    List<Material> materials = new List<Material>();
-    bool PingPong = false;
+    public List<Material> materials = new List<Material>();
+    public float dissolveSpeed = 1f;
+    private bool isDissolving = false;
+
     void Start()
     {
         var renders = GetComponentsInChildren<Renderer>();
-        for (int i = 0; i < renders.Length; i++)
+        foreach (var rend in renders)
         {
-            materials.AddRange(renders[i].materials);
+            materials.AddRange(rend.materials);
         }
     }
 
-    private void Reset()
+    public void OnButtonPress(Color color, float colorIntensity)
     {
-        Start();
-        SetValue(0);
+        if(isDissolving)
+            return;
+
+        isDissolving = true;
+        float targetValue = materials[0].GetFloat("_Dissolve") == 0f ? 1f : 0f;
+        StartCoroutine(DissolveTransition(targetValue, color, colorIntensity));
     }
 
-    void Update()
+    IEnumerator DissolveTransition(float targetValue, Color color, float colorIntensity)
     {
-       var value = Mathf.PingPong(Time.time * 0.5f, 1f);
-        SetValue(value);
-    }
+        float startValue = materials[0].GetFloat("_Dissolve");
+        float duration = Mathf.Abs(targetValue - startValue) / dissolveSpeed;
+        float elapsed = 0f;
 
-    public void SetValue(float value)
-    {
-        for (int i = 0; i < materials.Count; i++)
+        while (elapsed < duration)
         {
-            materials[i].SetFloat("_Dissolve", value);
+            elapsed += Time.deltaTime;
+            float currentValue = Mathf.Lerp(startValue, targetValue, elapsed / duration);
+            SetValue("_Dissolve", currentValue);
+            SetValue("_Color", color);
+            SetValue("_ColorIntensity", colorIntensity);
+            yield return null;
         }
+
+        SetValue("_Dissolve", targetValue);
+        isDissolving = false;
+    }
+
+    public void SetValue(string propertyName, float value)
+    {
+        foreach (var material in materials)
+        {
+            material.SetFloat(propertyName, value);
+        }
+    }
+
+    public void SetValue(string propertyName, Color value)
+    {
+        foreach (var material in materials)
+        {
+            material.SetColor(propertyName, value);
+        }
+    }
+
+    public void OnRedButtonPress()
+    {
+        OnButtonPress(Color.red, 1f);
+    }
+
+    public void OnGreenButtonPress()
+    {
+        OnButtonPress(Color.green, 1f);
+    }
+
+    public void OnBlueButtonPress()
+    {
+        OnButtonPress(Color.blue, 1f);
     }
 }
